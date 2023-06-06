ede5d8661405990a1129531aba18d026a2bb9caf
Tomas-Lefebvre <lefetomi@gmail.com>
2023-06-06 11:39:48 -0300

    Created_Project

---
+++ DEEP_DAV1D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump= true;
+            animator.SetBool("IsJumping", true);
+        }
+
+
+    }
+    
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+
+    }
+
+
+
+}
